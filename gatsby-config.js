b5b3840735b96af97ab303c40455120fb339a1d7
Ryo Kuroyanagi <ku6ryo@gmail.com>
2020-05-16 16:29:30 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `qpycs55a593d`,
+        accessToken: `7WMJ2jj7LpeCxe88tJKjLZqTnzEFt2VxMjmyA0CPgsk`,
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => {
+  return (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -14,8 +16,30 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <div>contentful</div>
+    <div>{data.allContentfulTestContent.edges.map(c => {
+      return <div key={c.node.title}>{c.node.title}</div>
+    })}</div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulTestContent {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
